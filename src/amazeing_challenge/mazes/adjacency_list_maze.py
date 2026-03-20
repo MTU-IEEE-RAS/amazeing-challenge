2f30816eeaca48e8d7ef47b5c31583d317c60443
Logan Schexnaydre <lpschexn@mtu.edu>
2026-03-19 21:53:52 -0400

    Finished comments.

--- src/amazeing_challenge/mazes/adjacency_list_maze.py
+++ src/amazeing_challenge/mazes/adjacency_list_maze.py
@@ -1,6 +1,12 @@
 from ..contracts import Maze
 
 class AdjacencyListMaze(Maze):
+    """Maze organized by an adjacency list
+
+    Args:
+        rows: the number of grid rows in a maze.
+        cols: the number of columns in a maze.
+    """
     def __init__(self,rows,cols):
         self.rows = rows
         self.cols = cols
@@ -11,6 +17,12 @@ class AdjacencyListMaze(Maze):
             self.adjacency_list[i] = [] 
 
     def add_edge(self, n1 : tuple, n2 : tuple):
+        """Add an edge into the maze.
+            
+            Args:
+                n1: First node (x,y).
+                n2: Second node (x,y).
+        """
 
         # Only add if nodes are neighbors
         if abs(n1[0] - n2[0]) + abs(n1[1]-n2[1]) > 1:
@@ -24,9 +36,25 @@ class AdjacencyListMaze(Maze):
         self.adjacency_list[n2_idx].append(n1_idx)
 
     def get_adj_list_idx(self, n: tuple) -> int:
+        """Return the index of a node in the adjacency list.
+
+            Args: 
+                n: node (x,y).
+            
+            Returns:
+                Index of node in adjacency list.
+        """
         return n[1] + n[0]*self.cols
     
     def get_edges(self,n:tuple) -> list:
+        """Return the edges of a node
+
+        Args:
+            n: node (x,y).
+
+        Returns:
+            A list of edges [(node 1, node 2),...].
+        """
         edges = []
         print(self.adjacency_list[self.get_adj_list_idx(n)])
         for v in self.adjacency_list[self.get_adj_list_idx(n)]:
@@ -36,12 +64,30 @@ class AdjacencyListMaze(Maze):
         return edges
 
     def get_rows(self) -> int:
+        """Return the number of rows.
+        """
         return self.rows
     
     def get_node_coordinate(self, v):
+        """Return the coordinates of an indexed node.
+
+        Args:
+            v: index of a node in the adjacency list.
+
+        Returns:
+            The 2D coordinate of the indexed node.
+        """
         return (v // self.cols, v % self.cols)
 
     def get_node_neighbors(self, n: tuple) -> list:
+        """Return all neighbors to a given node.
+
+        Args:
+            n: node.
+
+        Returns:
+            A list of nodes [(x,y)].
+        """
         neighbors = []
 
         left_idx = n[0] - 1
@@ -61,6 +107,8 @@ class AdjacencyListMaze(Maze):
         return neighbors
 
     def get_nodes(self) -> list:
+        """Return a list of the nodes.
+        """
 
         node_list = []
         for i in range(self.node_count):
@@ -68,19 +116,27 @@ class AdjacencyListMaze(Maze):
         return node_list
     
     def get_cols(self) -> int:
+        """Return the number of columns.
+        """
         return self.cols
 
     def has_edge(self,n1:tuple, n2:tuple) -> bool:
+        """Check if node 1 and node 2 are connected by an edge.
+        """
         v1 = self.get_adj_list_idx(n1)
         v2 = self.get_adj_list_idx(n2)
         
         return v1 in self.adjacency_list[v2]
     
     def print_adjacency_list(self):
+        """Prints the adjacency list.
+        """
         for vertex, neighbors in self.adjacency_list.items():
             print(f"{vertex} -> {' '.join(map(str, neighbors))}")
 
     def print_maze(self):
+        """Prints a maze vizualization.
+        """
 
         # The row and column numbers only print nicely for single digits for now...
         print_numbers = False
@@ -144,4 +200,6 @@ class AdjacencyListMaze(Maze):
         print(maze_string)
     
     def remove_edge(self, n1 : tuple, n2 : tuple):
+        """Removes an edge between node 1 and node 2.
+        """
         self.adjacency_list[n1].remove(n2)
